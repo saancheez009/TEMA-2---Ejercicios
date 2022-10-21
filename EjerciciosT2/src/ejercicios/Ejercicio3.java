54f698e68d32bc01eb483f42152bd174311adaf8
bsanchez <bsanchez@106-27.info2>
2022-10-21 09:41:42 +0200

    Commit

--- EjerciciosT2/src/ejercicios/Ejercicio3.java
+++ EjerciciosT2/src/ejercicios/Ejercicio3.java
@@ -4,33 +4,36 @@ import java.util.Scanner;
 
 public class Ejercicio3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Declaramos las variables comidacomprada, numAnimales, KilosComida,
-		double comidaComprada;
-		int numAnimales;
-		int kilosComida;
-		double racion ;
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Por favor introduzca la cantidad de comida comprada diariamente");
-		comidaComprada=sc.nextInt();
-		
-		System.out.println("Por favor introduzca el número de animales: ");
-		numAnimales=sc.nextInt();
-		
-		System.out.println("Por favor introduzca los kilos de comida que comen los animales en total: ");
-		kilosComida=sc.nextInt();
-		
-		racion=comidaComprada/numAnimales;
-		
-		if (comidaComprada==kilosComida || comidaComprada>kilosComida) {
-			System.out.println("Se dispone de alimento suficiciente para cada animal");
-		} else {
-			System.out.println("La ración para cada animal es: "+racion+" kg.");
-		}
-		sc.close();
-	}
-	
-}
+	//Para comprobar que el código es correcto introducimos una cantidad en kilos de comida comprada, iintroducimos 50 kilos, y luego el número de perros, 40 perros, finalmente , introducimos el total de comida que comen los perros que es 50, al ser 50 igual al número de comida comprada, el sistema nos devuelve que hay comida suficiente
+	// Luego hemos introducido un número de comida comprada menor a los kilos que comen los perros al día, con ello el sistema nos muestra que no hay suficiente comida y hace el calculo de la ración que le corresponde a cada perro, el calculo es la division de la comida comprada entre el número de perros totales.	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			//Declaramos las variables comidacomprada, numAnimales, KilosComida y racion
+			double comidaComprada; //Kilos de comida que el usuario ha comprado
+			int numAnimales; //Numero de perros totales que tiene el usuario
+			int kilosComida; // Kilos de comida que comen todos los animales al día
+			double racion ; //La ración que le pertenece a cada animal si no hay comida suficiente 
+			//Introducimos e importamos el scanner para poder leer el teclado
+			Scanner sc = new Scanner(System.in);
+			// Le pedimos al usuario que por favor introduzc la cantidad de comida comprada por el usuario	
+			System.out.println("Por favor introduzca la cantidad de comida comprada diariamente");
+			comidaComprada=sc.nextInt(); // Le pedimos al sistema que lea el número de kilos que ha comprado el usuario
+			// Le pedimos al usuario que por favor introduzca  el número de perros totales que tiene
+		    System.out.println("Por favor introduzca el número de animales: ");
+			numAnimales=sc.nextInt(); //Le decimos al sistema que lea el número de animales introducidos por el usuario
+			// Le pedimos al usuario que por favor introduzca los kilos de comida que comen los perros en total		
+			System.out.println("Por favor introduzca los kilos de comida que comen los animales en total: ");
+			kilosComida=sc.nextInt(); //Le decimos al sistema que lea el número de kilos que comen los perros introducido por el usuario
+			// Calculamos la racion que le corresponde a cada animal, dividiendo el numero de comida comprada entre el número de animales
+			racion=comidaComprada/numAnimales;
+			// Declaramos que si el número de comida comprada y los kilos de comida que comen en total son iguales, le decimos al usuario que tiene comida suficiente
+			if (comidaComprada==kilosComida || comidaComprada>kilosComida) {
+				System.out.println("Se dispone de alimento suficiciente para cada animal");
+			} else { //Si la condición anterior no se cumple le decimos al usuario que no tiene comida suficiente y le calculamos la ración para cada animal
+				System.out.println("La ración para cada animal es: "+racion+" kg.");
+			}
+					sc.close(); //Cerramos el scanner
+				}
+				
+			}
+
--- EjerciciosT2/src/ejercicios/Ejercicio4.java
+++ EjerciciosT2/src/ejercicios/Ejercicio4.java
@@ -4,138 +4,178 @@ import java.util.Scanner;
 
 public class Ejercicio4 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int num;
-		int num1;
-		int num2;
-		String letra=null;
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Por favor introduzca un número comprendido entre 1 y 99");
-		num=sc.nextInt();
-		if(num<16) {
-		switch (num) {
-		case 1:
-			letra=("uno");
-			break;
-		case 2:
-			letra=("dos");
-			break;
-		case 3:
-			letra=("tres");
-			break;
-		case 4:
-			letra=("cuatro");
-			break;
-		case 5:
-			letra=("cinco");
-			break;
-		case 6:
-			letra=("seis");
-			break;
-		case 7:
-			letra=("siete");
-			break;
-		case 8:
-			letra=("ocho");
-			break;
-		case 9:
-			letra=("nueve");
-			break;
-		case 10:
-			letra=("diez");
-			break;
-		case 11:
-			letra=("once");
-			break;
-		case 12:
-			letra=("doce");
-			break;
-		case 13:
-			letra=("trece");
-			break;
-		case 14:
-			letra=("catorce");
-			break;
-		case 15:
-			letra=("quince");	
-			break;
-		
-	} 
-		} else {
-			num1= (int) num/10;
-			num2=num%10;
-		
-		switch (num1) {
+	// Para comprobar que el código es correcto, hemos introducido números del 10 al 29 y nos ha devuelto el número introducido correctamente
+	// Luego hemos introducidos varios nuemros a partir del 30 y nos ha devuelto el número escrito correctamente
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			//Declaramos las variables num , num1 y num2
+			int num; //Número introducido por el usuario
+			int num1; //Primer número del número que ha introducido el usuario , es decir si el usuario introduce 78, el num1 es 7
+			int num2; //Segundo número del número que ha introducido elusuario, si ha introducido 56, el num2 es 6
+			String letra=null; //número escrito que se mostrará al usuario
+			//Introducimos e importamos el scanner para leer el teclado
+			Scanner sc = new Scanner(System.in);
+			// Le pedimos al usuario que introduzca un número comprendido entre 1 y 99
+			System.out.println("Por favor introduzca un número comprendido entre 1 y 99");
+			num=sc.nextInt(); //Leemos el numero intoducido por el usuario 
+			if(num<29) { //Declaramos que si el número es menor a 29 se lee como asignamos a cada numero mediante switch
+			switch (num) {
 			case 1:
-				letra="dieci";
+				letra=("uno");
 				break;
 			case 2:
-				letra="veinti y";
+				letra=("dos");
 				break;
 			case 3:
-				letra="treinta";
+				letra=("tres");
 				break;
 			case 4:
-				letra="cuarenta ";
+				letra=("cuatro");
 				break;
 			case 5:
-				letra="cincuenta";
+				letra=("cinco");
 				break;
 			case 6:
-				letra="sesenta";
+				letra=("seis");
 				break;
 			case 7:
-				letra="setenta";
+				letra=("siete");
 				break;
 			case 8:
-				letra="ochenta";
+				letra=("ocho");
 				break;
 			case 9:
-				letra="noventa";
+				letra=("nueve");
+				break;
+			case 10:
+				letra=("diez");
+				break;
+			case 11:
+				letra=("once");
+				break;
+			case 12:
+				letra=("doce");
+				break;
+			case 13:
+				letra=("trece");
+				break;
+			case 14:
+				letra=("catorce");
+				break;
+			case 16:
+				letra=("dieciseis");	
+				break;
+			case 17:
+				letra=("diecisiete");
+				break;
+			case 18:
+				letra=("dieciocho");
+				break;
+			case 19:
+				letra=("diecinueve");
+				break;
+			case 21:
+				letra=("veintiuno");
+				break;
+			case 22:
+				letra=("veintidos");	
+				break;
+			case 23:
+				letra=("veintitres");
+				break;
+			case 24:
+				letra=("veinticuatro");
+				break;
+			case 25:
+				letra=("veinticinco");
+				break;
+			case 26:
+				letra=("veintiseis");
+				break;
+			case 27:
+				letra=("veintisiete");	
+				break;
+			case 28:
+				letra=("veintiocho");
+				break;
+			case 29:
+				letra=("veintinueve");
 				break;
 			
-				}
-		switch (num2) {
 			
-		case 1:
-			letra+=(" y uno");
-			break;
-		case 2:
-			letra+=(" y dos");
-			break;
-		case 3:
-			letra+=(" y tres");
-			break;
-		case 4:
-			letra+=(" y cuatro");
-			break;
-		case 5:
-			letra+=(" y cinco");
-			break;
-		case 6:
-			letra+=(" y seis");
-			break;
-		case 7:
-			letra+=(" y siete");
-			break;
-		case 8:
-			letra+=(" y ocho");
-			break;
-		case 9:
-			letra+=(" y nueve");
-			break;
-		case 0:
-			letra+="";
-			break;
+		}  
+			} else {
+				num1= (int) num/10; //Esto nos mostrara el primer numero del numero, si es 89, el num1 es 8
+				num2=num%10; // Esto nos muestra el segundo numero del numero introducido, por ejemplo si es 67, nos muestra 7
+			// Le damos a los num1 un valor de una letra mediante switch
+			switch (num1) {
+				case 3:
+					letra="treinta";
+					break;
+				case 4:
+					letra="cuarenta ";
+					break;
+				case 5:
+					letra="cincuenta";
+					break;
+				case 6:
+					letra="sesenta";
+					break;
+				case 7:
+					letra="setenta";
+					break;
+				case 8:
+					letra="ochenta";
+					break;
+				case 9:
+					letra="noventa";
+					break;
+				
+					}
+			switch (num2) { //Le damos al num2 un valor de una letra mediante un switch, este numero se agrega al num1
+				
+			case 1:
+				letra+=(" y uno");
+				break;
+			case 2:
+				letra+=(" y dos");
+				break;
+			case 3:
+				letra+=(" y tres");
+				break;
+			case 4:
+				letra+=(" y cuatro");
+				break;
+			case 5:
+				letra+=(" y cinco");
+				break;
+			case 6:
+				letra+=(" y seis");
+				break;
+			case 7:
+				letra+=(" y siete");
+				break;
+			case 8:
+				letra+=(" y ocho");
+				break;
+			case 9:
+				letra+=(" y nueve");
+				break;
+			case 0:
+				letra+="";
+				break;
+				
+			}
+			} // En una excepción declaramos que si el usuario introduce 20 le devolvemos el número 20 escrito
+			if(num==20) {
+				letra="veinte";
+			} //Le mostramos al usuario el número que ha introducido escrito
+			System.out.println(letra);
 			
-		}
-		}
-		if(num==20) {
-			letra="veinte";
-		}
-		System.out.println(letra);
+		
+		sc.close(); //cerramos el scanner
+
 	}
 }
